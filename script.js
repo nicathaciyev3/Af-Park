1875ccc26ce332b028ca00d4ec0e1be45795ef7e
Nicat Haciyev <115214478+nicathaciyev3@users.noreply.github.com>
2026-09-02 13:10:52 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const videos = document.querySelectorAll("video");
+
+videos.forEach(video => {
+
+    video.addEventListener("play", () => {
+
+        videos.forEach(otherVideo => {
+
+            if (otherVideo !== video) {
+                otherVideo.pause();
+            }
+
+        });
+
+    });
+
+});
